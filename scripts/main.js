8f73c254206604fbef60a409737aee5ebda1f0a0
Dominic Evans <dominic@bitt.com>
2019-04-20 20:20:09 -0400

    - name change - pictures of me - update skills

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,12 @@
 window.onload = function(){ 
-	
-	var htmlcss = new JustGage({
-    id: "htmlGauge",
-    value: 90,
-    min: 0,
-    max: 100,
-    title: "Html/CSS"
-  });
-
-	var javascript = new JustGage({
-    id: "javascriptGauge",
-    value: 70,
-    min: 0,
-    max: 100,
-    title: "JavaScript"
-  });
-
-
-	var PHP = new JustGage({
-    id: "phpGauge",
-    value: 60,
-    min: 0,
-    max: 100,
-    title: "PHP"
-  });
-
-	var mySQL = new JustGage({
-    id: "mySQLGauge",
-    value: 60,
-    min: 0,
-    max: 100,
-    title: "mySQL"
-  });
-
-	var PSD = new JustGage({
-    id: "PSDGauge",
-    value: 80,
-    min: 0,
-    max: 100,
-    title: "Adobe Photoshop"
-  });
-
-	var Illustrator = new JustGage({
-    id: "illustratorGauge",
-    value: 80,
-    min: 0,
-    max: 100,
-    title: "Adobe Illustrator"
-  });
-
-   
-
-
+  setInterval(changeImg, 2500)
 }
 
-   
+let index = 0;
+
+function changeImg() {
+  [].forEach.call(document.getElementsByClassName("me"), (v, i) =>{
+    document.getElementsByClassName("me")[i].hidden = i !== index
+  });
+  index = (index + 1) % document.getElementsByClassName("me").length;
+}
